07c48b2cd313b6328d98c992100b02aace38e0df
Refayat Haque <rhaque@pyramidsystems.com>
2021-02-26 13:22:58 -0500

    Feb 26 13:22

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,12 +1,24 @@
 <template>
-  <section><coach-filter @change-filter="setFilters"></coach-filter></section>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+  <!-- since the `show` directive wants a boolean we need to force a boolean out of the `error` data property (which can be null or a string) using !! (using just 1 ! would be the opposite, and we don't want that here) -->
+    {{ error }}
+  </base-dialog>
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register">Register as coach</base-button>
+        <base-button mode="outline" @click="handleLoadCoaches"
+          >Refresh</base-button
+        >
+        <base-button v-if="!isCoach && !isLoading" link to="/register"
+          >Register as coach</base-button
+        >
+        <!-- register button should only display if the user is not a registered coach and also when data is NOT loading -->
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading"><base-spinner></base-spinner></div>
+      <ul v-else-if="handleHasCoaches">
         <li v-for="coach in filteredCoaches" :key="coach.id">
           <!-- https://v3.vuejs.org/style-guide/#prop-name-casing-strongly-recommended -->
           <coach-item
@@ -24,15 +36,19 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 import CoachItem from "../../components/coaches/CoachItem.vue";
 import BaseCard from "../../components/ui/BaseCard.vue";
 import BaseButton from "../../components/ui/BaseButton.vue";
 import CoachFilter from "../../components/coaches/CoachFilter.vue";
+import BaseSpinner from "../../components/ui/BaseSpinner.vue";
+import BaseDialog from '../../components/ui/BaseDialog.vue';
 export default {
-  components: { CoachItem, BaseCard, BaseButton, CoachFilter },
+  components: { CoachItem, BaseCard, BaseButton, CoachFilter, BaseSpinner, BaseDialog },
   data() {
     return {
+      isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -42,6 +58,10 @@ export default {
   },
   computed: {
     ...mapGetters("coachesModule", ["coaches", "hasCoaches", "isCoach"]),
+    handleHasCoaches() {
+      return !this.loading && this.hasCoaches;
+      // want to make sure that we return true only if there are coaches AND when data is NOT loading
+    },
     filteredCoaches() {
       return this.coaches.filter((coach) => {
         if (this.activeFilters.frontend && coach.areas.includes("frontend")) {
@@ -64,6 +84,22 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    async handleLoadCoaches() {
+      this.isLoading = true;
+      try {
+        await this.loadCoaches();
+      } catch (error) {
+        this.error = error.message || "Something went wrong";
+      }
+      this.isLoading = false;
+    },
+    ...mapActions("coachesModule", ["loadCoaches"]),
+    handleError() {
+      this.error = null
+    }
+  },
+  created() {
+    this.handleLoadCoaches();
   },
 };
 </script>
